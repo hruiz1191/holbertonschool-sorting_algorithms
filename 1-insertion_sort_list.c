4bf9d017220afa3f8c21eae82fe8d5bd9604573e
hruiz <elproferuiz@yahoo.com>
2024-12-14 19:47:37 -0400

    task1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function to sort doubly linked list
+ * @list: head of the list
+ **/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *current, *next, *prev;
+
+	if (list && *list)
+	{
+		temp = (*list)->next;
+		while (temp)
+		{
+			current = temp;
+			prev = current->prev;
+			temp = temp->next;
+			while (prev && prev->n > current->n)
+			{
+				next = current->next;
+				current->next = prev;
+				current->prev = prev->prev;
+				prev->next = next;
+				if (next)
+					next->prev = prev;
+				if (prev->prev)
+					prev->prev->next = current;
+				else
+					*list = current;
+				prev->prev = current;
+				print_list(*list);
+				prev = current->prev;
+			}
+		}
+	}
+}
